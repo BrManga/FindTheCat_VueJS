7dd794b68609b66158656879e298d1e367cd2444
Bora Manga <bora.mng525@gmail.com>
2020-06-15 16:22:56 +0200

    completed

--- src/components/Failure.vue
+++ src/components/Failure.vue
@@ -1,6 +1,48 @@
-<template></template>
+<template>
+  <div class="container">
+    <h1>Wrong Answer!!!</h1>
+    <button @click="newGame">New Game</button>
+  </div>
+</template>
 <script>
-export default {};
+export default {
+  methods: {
+    newGame() {
+      this.$emit("isCorrectEvent", "appGameCards");
+    }
+  }
+};
 </script>
-<style>
-</style>
+<style scoped>
+.container {
+  position: absolute;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: darkred;
+}
+.container h1 {
+  text-align: center;
+  font-size: 333%;
+  color: white;
+}
+.container button {
+  cursor: pointer;
+  width: 200px;
+  color: darkred;
+  border: 2px solid black;
+  font-size: 150%;
+}
+.container button:hover {
+  transition: 0.5s;
+  color: rgb(198, 223, 198);
+  background: lightsalmon;
+}
+</style>>
+
--- src/components/GameCards.vue
+++ src/components/GameCards.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="game-area">
-    <p>{{selectedCard}}</p>
+    <p>{{answer}}</p>
     <h1 class="title">Where is the cat?</h1>
     <h4 class="description">Guess the card</h4>
     <div class="container">
-      <appCard
-        :class="{'shadow':selectedCard==card.id}"
-        v-for="card in cards"
-        :key="card.id"
-        :card="card"
-        @click.native="selectedCard=card.id"
-      ></appCard>
+      <transition-group name="rotate-all" class="card-container" appear>
+        <appCard
+          :class="{'shadow':selectedCard==card.id}"
+          v-for="card in cards"
+          :key="card.id"
+          :card="card"
+          @click.native="selectedCard=card.id"
+        ></appCard>
+      </transition-group>
     </div>
     <div class="container">
-      <appDefaultCard :card="answer"></appDefaultCard>
+      <transition name="rotate" mode="out-in">
+        <component :is="activeCard" @click.native="showCard(answer)" :card="answer"></component>
+      </transition>
     </div>
   </div>
 </template>
@@ -23,20 +27,38 @@ import DefaultCard from "./DefaultCard";
 export default {
   data: function() {
     return {
-      selectedCard: "",
+      selectedCard: null,
       answer: {},
+      activeCard: "app-default-card",
       cards: [
-        { id: 1, component: "app-cards", image: "/src/assets/card-1.jpg" },
-        { id: 2, component: "app-cards", image: "/src/assets/card-2.jpg" },
-        { id: 3, component: "app-cards", image: "/src/assets/card-3.jpg" },
-        { id: 4, component: "app-cards", image: "/src/assets/card-4.jpg" },
-        { id: 5, component: "app-cards", image: "/src/assets/card-5.jpg" }
+        { id: 1, component: "app-card", image: "/src/assets/card-1.jpg" },
+        { id: 2, component: "app-card", image: "/src/assets/card-2.jpg" },
+        { id: 3, component: "app-card", image: "/src/assets/card-3.jpg" },
+        { id: 4, component: "app-card", image: "/src/assets/card-4.jpg" },
+        { id: 5, component: "app-card", image: "/src/assets/card-5.jpg" }
       ]
     };
   },
   created() {
     let answer = Math.floor(Math.random() * this.cards.length);
     this.answer = this.cards[answer];
+  },
+  methods: {
+    showCard(answer) {
+      if (this.selectedCard == null) {
+        alert("Please select a card");
+      } else {
+        this.activeCard = this.answer.component;
+        setTimeout(() => {
+          if (answer.id == this.selectedCard) {
+            //@isCorrectEvent="activeComponent=$event" emitting to upper component the value with $event
+            this.$emit("isCorrectEvent", "appCelebrate");
+          } else {
+            this.$emit("isCorrectEvent", "appFailure");
+          }
+        }, 2000);
+      }
+    }
   },
   components: {
     appCard: Card,
@@ -59,8 +81,59 @@ export default {
   align-items: center;
   margin-top: 50px;
 }
+.card-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+}
 .shadow {
   box-shadow: 0px 5px 48px #30968f !important;
   transition: box-shadow 1s;
+}
+/* Acik kart animasyon  */
+.rotate-all-enter {
+}
+.rotate-all-enter-active {
+  animation: rotate-all ease-in-out 2s forwards;
+}
+.rotate-all-leave {
+}
+.rotate-all-leave-active {
+}
+@keyframes rotate-all {
+  from {
+    transform: rotateY(0);
+  }
+  to {
+    transform: rotateY(1080deg);
+  }
+}
+/* Kapali kart animasyon  */
+.rotate-enter {
+}
+.rotate-enter-active {
+  animation: rotate-in ease-in-out 1s forwards;
+}
+.rotate-leave {
+}
+.rotate-leave-active {
+  animation: rotate-out ease-in-out 1s forwards;
+}
+@keyframes rotate-in {
+  from {
+    transform: rotateY(90deg);
+  }
+  to {
+    transform: rotateY(0deg);
+  }
+}
+@keyframes rotate-out {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(90deg);
+  }
 }
 </style>
